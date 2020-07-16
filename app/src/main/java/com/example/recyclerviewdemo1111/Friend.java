253bb0b42faab1fc9f98080c360ce43c1f4c8f22
BVCN 88 CS2 <daothanhcong70@gmail.com>
2020-07-16 09:04:53 +0700

    tạo models Friend

---
+++ app/src/main/java/com/example/recyclerviewdemo1111/Friend.java
@@ -0,0 +1,27 @@
+package com.example.recyclerviewdemo1111;
+
+public class Friend {
+    private String name;
+    private String phone;
+
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
